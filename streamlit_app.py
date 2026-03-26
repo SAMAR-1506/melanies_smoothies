83c664cc807f7f59056ca926abdac3bdabcb0e75
SAMAR-1506 <samarendra.sahoo@kpipartners.com>
2026-03-27 00:00:44 +0530

    Update streamlit_app.py
    smoothiefroot nutrition informations details added

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,11 @@ ingredients_list = st.multiselect(
     ,max_selections=5
 )
 
+#New Section added to diaplay smoothiefroot nutrition informations.
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
