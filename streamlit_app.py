02936f9ad14729c6c98405c915f91b01eda13f63
SAMAR-1506 <samarendra.sahoo@kpipartners.com>
2026-03-27 01:05:13 +0530

    Update streamlit_app.py
    Add Panda Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,10 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')) 
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
+#Convert the snowpark DataFrame to a pandas DataFrame so wwe can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:',
     my_dataframe
