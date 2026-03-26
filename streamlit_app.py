c59cbb1b07c064ef62a2fd6c332279ca4b6e6d83
SAMAR-1506 <samarendra.sahoo@kpipartners.com>
2026-03-27 00:14:21 +0530

    Update streamlit_app.py
    fruit name change from watermelon to Watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ ingredients_list = st.multiselect(
 
 #New Section added to diaplay smoothiefroot nutrition informations.
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/Watermelon)")  
 st.text(smoothiefroot_response.json())
 st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
